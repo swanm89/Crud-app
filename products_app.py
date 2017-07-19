657fef06d0be7ad5d56a4e95391eb0ff74990669
Michael Swan <mfs370@nyu.com>
2017-07-19 19:40:55 -0400

    Finished checkpoint 1

--- products_app.py
+++ products_app.py
@@ -12,17 +12,26 @@ menu = """
 """
 
 chosen_operation = input(menu)
+chosen_operation = chosen_operation.title()
 
-if chosen_operation.title() == "List":
+def list_products():
     print("LISTING PRODUCTS")
-elif chosen_operation.title() == "Show":
+
+def show_product():
     print("SHOWING A PRODUCT")
-elif chosen_operation.title() == "Create":
+
+def create_product():
     print("CREATING A PRODUCT")
-elif chosen_operation.title() == "Update":
+
+def update_product():
     print("UPDATING A PRODUCT")
-elif chosen_operation.title() == "Destroy":
+
+def destroy_product():
     print("DESTROYING A PRODUCT")
-else:
-    print("OOPS. PLEASE CHOOSE ONE OF THE RECOGNIZED OPERATIONS.")
-    print("Hello")
+
+if chosen_operation == "List": list_products()
+elif chosen_operation == "Show": show_product()
+elif chosen_operation == "Create": create_product()
+elif chosen_operation == "Update": update_product()
+elif chosen_operation == "Destroy": destroy_product()
+else: print("OOPS. PLEASE CHOOSE ONE OF THE RECOGNIZED OPERATIONS.")
